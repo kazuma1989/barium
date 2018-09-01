2943b52b4c08c9110e0cdb8fd3617b29ae8157e5
kazuma1989 <funifuni.1204@gmail.com>
2018-09-01 10:39:16 +0900

    refactor: 弱いビビり係数は一旦除去
    最大得点の値を共通化した。計算箇所によって少しずつ違う値を返していたため。

--- src/sampleAi2.js
+++ src/sampleAi2.js
@@ -8,6 +8,8 @@ Object.assign(PieceType, {
   caughtLion: 16,
 });
 
+const MAX_POINT = 1000000;
+
 const rule = new Map([
   [PieceType.chick, 100],
   [PieceType.chicken, 1200],
@@ -17,7 +19,7 @@ const rule = new Map([
   [PieceType.lion, 10000],
   [PieceType.chickInEnemyArea, 600],
   [PieceType.catInEnemyArea, 1100],
-  [PieceType.caughtLion, 1000000],
+  [PieceType.caughtLion, MAX_POINT],
 ]);
 
 // 状態を評価します。
@@ -45,16 +47,16 @@ function evaluate(state) {
     }
 
     let bibiri = 1.0;
-    if (state.player === Player.white) {
-      if (piece.owner === Player.white && board[index + 14]) {
-        bibiri = board[index + 14].owner === Player.black ? 1.5 : 1.0;
-      }
-    }
-    if (state.player === Player.black) {
-      if (piece.owner === Player.black && board[index - 14]) {
-        bibiri = board[index - 14].owner === Player.white ? 1.5 : 1.0;
-      }
-    }
+    // if (state.player === Player.white) {
+    //   if (piece.owner === Player.white && board[index + 14]) {
+    //     bibiri = board[index + 14].owner === Player.black ? 1.5 : 1.0;
+    //   }
+    // }
+    // if (state.player === Player.black) {
+    //   if (piece.owner === Player.black && board[index - 14]) {
+    //     bibiri = board[index - 14].owner === Player.white ? 1.5 : 1.0;
+    //   }
+    // }
 
     return acc + rule.get(type) * bibiri * player;
   }, 0);
@@ -109,7 +111,7 @@ function getScore(state, depth, alpha, beta) {
 
   // 勝者が確定している場合は、探索を止めて大きな値を返します。
   if (state.winner) {
-    return state.winner === state.player ? 100000 : -100000;
+    return state.winner === state.player ? MAX_POINT : -MAX_POINT;
   }
 
   // 指定した深さまで探索した場合は、盤面を評価してスコアとします。
@@ -136,11 +138,11 @@ function getScore(state, depth, alpha, beta) {
 // 手を返せるように、スコアと手の配列をaccにするアルファ・ベータ法（正しくはネガ・アルファ法）を実行します。getScoreを手も扱うように改造すればコードの重複がなくなるのですけど、配列の生成は遅そうなので、敢えてこんなコードにしてみました。
 export function getMove(state) {
   const [score, move] = reduce((acc, move) => {
-    const score = -getScore(state.doMove(move), 4, -1000000, -acc[0]);  // 5手読むとすげー遅かったので、読む深さは4手で。
+    const score = -getScore(state.doMove(move), 4, -MAX_POINT, -acc[0]);  // 5手読むとすげー遅かったので、読む深さは4手で。
     const nextAcc = score > acc[0] ? [score, move] : acc;  // TODO: スコアが同じ場合にランダムで入れ替えると、手に幅がでて良いかも。
 
     return nextAcc;
-  }, [-1000000, null], state.getLegalMoves());  // TODO: 合法手が全く無い場合にnullが返るけど、大丈夫？　合法手が全くない状態が想像できないけど……。
+  }, [-MAX_POINT, null], state.getLegalMoves());  // TODO: 合法手が全く無い場合にnullが返るけど、大丈夫？　合法手が全くない状態が想像できないけど……。
 
   console.log(score);
 
